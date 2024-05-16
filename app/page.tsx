27b4e5bf2299fa575428a221637eb2252f973af1
Matheus <matheusfariasdasneves@gmail.com>
2024-05-15 22:34:25 -0300

    feat: add promo banner 01 to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search-input";
@@ -6,12 +7,25 @@ const Home = () => {
   return (
     <div>
       <Header />
+
       <div className="px-5 pt-6">
         <Search />
       </div>
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full object-contain"
+          quality={100}
+        />
       </div>
     </div>
   );
