6ae2db61d42ebb8638a8ff317f81ecbc615f5090
Matheus <matheusfariasdasneves@gmail.com>
2024-05-19 10:36:01 -0300

    fix: remove async from product list

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -14,7 +14,7 @@ interface ProductListProps {
   }>[];
 }
 
-const ProductList = async ({ products }: ProductListProps) => {
+const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex gap-4 overflow-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
