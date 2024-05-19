4fd92c8cd44ef169785b0c1abf09ee2cf6532b55
Matheus <matheusfariasdasneves@gmail.com>
2024-05-19 17:01:19 -0300

    feat: ensure click on logo return to home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="flex justify-between px-5 pt-6">
-      <Image
-        src="/logo.png"
-        alt="FSW Foods"
-        height={0}
-        width={0}
-        sizes="100vw"
-        className="h-8 w-28"
-        priority
-      />
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          alt="FSW Foods"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-8 w-28"
+          priority
+        />
+      </Link>
 
       <Button
         size="icon"
