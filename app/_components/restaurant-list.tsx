fbaad16a8bb861b160994ffa283bf850edebb877
Matheus <matheusfariasdasneves@gmail.com>
2024-06-23 18:57:36 -0300

    fix: add userFavoriteRestaurants in RestaurantList component

--- app/_components/restaurant-list.tsx
+++ app/_components/restaurant-list.tsx
@@ -1,13 +1,24 @@
+import { getServerSession } from "next-auth";
 import { db } from "../_lib/prisma";
 import RestaurantItem from "./restaurant-item";
+import { authOptions } from "../_lib/auth";
 
 const RestaurantList = async () => {
+  const session = await getServerSession(authOptions);
+
   const restaurants = await db.restaurant.findMany({ take: 10 });
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: { userId: session?.user?.id },
+  });
 
   return (
-    <div className="flex gap-4 overflow-scroll px-5 [&::-webkit-scrollbar]:hidden">
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {restaurants.map((restaurant) => (
-        <RestaurantItem key={restaurant.id} restaurant={restaurant} />
+        <RestaurantItem
+          key={restaurant.id}
+          restaurant={restaurant}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
       ))}
     </div>
   );
